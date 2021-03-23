391c2fedd54c1f1d2855dbc3db57199796a55aa4
Merouane <46603154+wane75@users.noreply.github.com>
2021-03-23 23:25:53 +0100

    expose Job type

--- lib/index.ts
+++ lib/index.ts
@@ -2,9 +2,10 @@ export * from "./agenda";
 export * from "./job";
 
 export { DefineOptions, JobPriority, Processor } from "./agenda/define";
+
+export { Job } from './job'
 export { JobOptions } from "./job/repeat-every";
 
 import { Agenda } from "./agenda";
 export { Agenda };
 export default Agenda;
-
