96462b72026550f714b796512563c03fae24e820
Aurélien Boquet <aurelien.boquet.web@gmail.com>
2021-12-23 14:54:51 +0100

    add build and maybe fix some heroku issues we had

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+export * from "./agenda";
+export * from "./job";
+export { DefineOptions, JobPriority, Processor } from "./agenda/define";
+export { Job } from './job';
+export { JobOptions } from "./job/repeat-every";
+import { Agenda } from "./agenda";
+export { Agenda };
+export default Agenda;
+//# sourceMappingURL=index.d.ts.map
