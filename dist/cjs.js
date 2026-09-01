1c352876ff084fdce1a342830b1c3bd7a8635a92
xenokratos <wane.75@outlook.com>
2026-09-02 00:44:21 +0200

    build: track dist and bump version to 5.0.0-beedeez.1
    Consumers install this fork straight from git and there is no prepare script,
    so the build has to be committed.

---
+++ dist/cjs.js
@@ -0,0 +1,29 @@
+"use strict";
+// old common js export (see index.ts for module exports)
+// Keeps the agenda@4 surface: consumers do `import Agenda, { Job, JobPriority } from "agenda"`,
+// and JobPriority is a runtime enum, so it has to stay on module.exports.
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const agenda_1 = require("./agenda");
+const api = __importStar(require("./index"));
+module.exports = agenda_1.Agenda;
+Object.assign(module.exports, api);
+//# sourceMappingURL=cjs.js.map
